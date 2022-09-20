62ac1d44c73589cfe624b7f163b13aeae4e2ffe5
unknown <jessc@yandex.ru>
2022-09-20 17:32:54 +0300

    added  models and postgres

---
+++ models/database.go
@@ -0,0 +1,17 @@
+    
+package models
+
+import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+	)
+
+func ConnectDB() *gorm.DB {
+	db, err := gorm.Open("postgres", "host=127.0.0.1 port=5432 user=postgres dbname=musicstore password=postgres sslmode=disable")
+	if err != nil {
+		panic("Не удалось подключиться к базе данных")
+	}
+	db.AutoMigrate(&Track{})
+
+	return db
+}
